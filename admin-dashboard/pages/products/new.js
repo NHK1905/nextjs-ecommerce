c9d4a96b77e37c7797e429643fc8a766557a8421
NHK1905 <nguyenhuykhang195@gmail.com>
2023-07-02 17:59:08 +0700

    Update Add, Edit, Delete, Upload Img,...

--- admin-dashboard/pages/products/new.js
+++ admin-dashboard/pages/products/new.js
@@ -1,52 +1,12 @@
 import Layout from "@/components/Layout";
-import axios from "axios";
-import { useRouter } from "next/router";
-import { useState } from "react";
+import ProductForm from "@/components/ProductForm";
+
 
 export default function NewProducts() {
-    const [title, setTitle] = useState('')
-    const [description, setDescription] = useState('')
-    const [price, setPrice] = useState('')
-    const [goToProducts, setGoToProducts] = useState(false)
-    const router = useRouter()
-
-    async function createProduct(ev) {
-        ev.preventDefault();
-        const data = { title, description, price }
-        await axios.post('../api/products', data)
-        setGoToProducts(true)
-    }
-    if (goToProducts) {
-        router.push('/products')
-    }
-
     return (
         <Layout>
-            <form onSubmit={createProduct}>
-                <h1 className="text-blue-900 mb-2 text-xl">New Product</h1>
-                <label>Product Name</label>
-                <input type="text"
-                    placeholder="Product Name"
-                    value={title}
-                    onChange={ev => setTitle(ev.target.value)}
-                />
-                <label>Description</label>
-                <textarea
-                    placeholder="Description"
-                    value={description}
-                    onChange={ev => setDescription(ev.target.value)}
-                />
-                <label>Price (in USD)</label>
-                <input
-                    type="text"
-                    placeholder="Price"
-                    value={price}
-                    onChange={ev => setPrice(ev.target.value)}
-                />
-                <button
-                    type="summit"
-                    className="btn-primary">Save</button>
-            </form>
+            <h1 className="text-blue-900 mb-2 text-xl">New Product</h1>
+            <ProductForm />
         </Layout>
     )
 }
